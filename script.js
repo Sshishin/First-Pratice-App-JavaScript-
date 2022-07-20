dd159efb7a3ae421aeed233e87bee9fe42a52509
Sergey Shishin <flyers32rus@gmail.com>
2022-07-20 12:09:15 +0300

    question automation pratice app #2

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 'use strict';
-
+//Объявили переменные под весь проект
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+let RatingOfWatch;
+let QuestOfWatch;
 
+// Запросили ответ у пользователя
 alert(numberOfFilms);
 
-let firtsQuestOfWatch = prompt('Один из последних просмотренных фильмов','')
-
-let firstRatingOfWatch = prompt('На сколько оцените его','')
-
-let secondQuestOfWatch = prompt('Один из последних просмотренных фильмов','')
-
-let secondRatingOfWatch = prompt('На сколько оцените его','')
-
+//Создали массив
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -20,7 +16,32 @@ let personalMovieDB = {
     privat: false
 };
 
-personalMovieDB.movies[firtsQuestOfWatch] = firstRatingOfWatch  //Присваиваем ключу его свойство
-personalMovieDB.movies[secondQuestOfWatch] = secondRatingOfWatch
+//  Создали цикл для автоматизации вопросов для пользователей
+for(let i = 1; i <= 2; i++) {
+    QuestOfWatch = prompt('Один из последних просмотренных фильмов','');
+    RatingOfWatch = prompt('На сколько оцените его','');
 
-console.log(personalMovieDB)
+       if(QuestOfWatch != '' && RatingOfWatch != '' && QuestOfWatch != null && RatingOfWatch != null && QuestOfWatch.length < 50) {
+        console.log ('Все хорошо');
+        personalMovieDB.movies[QuestOfWatch] = RatingOfWatch; //Добавляем элемент в массив если все хорошо и соединяем название и рейтинг
+       } else {
+        console.log('Еще раз');
+        i--; //Иначе повторяем вопрос
+       }
+
+}
+
+// Выводим сообщение в зависимости от первого вопроса про кол-во фильмов
+if(personalMovieDB['count'] < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB['count'] < 30) {
+    console.log('Вы классный зритель'); 
+} else if (personalMovieDB['count'] > 30) {
+    console.log('Вы киноман'); 
+} else {
+    console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
+
+
